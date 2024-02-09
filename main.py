50c8a8d76a0c51763fecc472b43fa68a56e9c509
master-da02 <mahdi.fpg@gmail.com>
2024-02-09 16:38:00 +0600

    added stations

--- main.py
+++ main.py
@@ -16,5 +16,19 @@ def create_user():
 
     return data, 201
 
+@app.route('/api/stations', methods=['POST'])
+def create_station():
+
+    data = request.get_json()
+
+    db = sqlite3.connect('sqlite.db')
+    cursor = db.cursor()
+    cursor.execute('INSERT INTO stations (station_id, station_name, longitude, latitude) VALUES (?, ?, ?, ?)', (data['station_id'], data['station_name'], data['longitude'], data['latitude']))
+    db.commit()
+    db.close()
+
+    return data, 201
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS users;
+DROP TAbLE IF EXISTS stations;
 
 create table users (
   user_id INTEGER primary key,
   user_name varchar(255) not null,
   balance INTEGER not null
 );
+
+create table stations (
+    station_id INTEGER primary key,
+    station_name VARCHAR(255) not null,
+    longitude FLOAT not null,
+    latitude FLOAT not null
+)
